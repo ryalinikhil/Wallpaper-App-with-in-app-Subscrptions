d5a867928e976d3d47f2233e87c3a815f7f2f36d
ryalinikhil <sameernikhil1234@gmail.com>
2023-10-09 15:39:38 +0530

    Initial Commit

---
+++ Feng Wallpaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Feng Wallpaper
+//
+//  Created by Sameer Nikhil on 09/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Feng Wallpaper/Feng_WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feng_WallpaperApp.swift
+//  Feng Wallpaper
+//
+//  Created by Sameer Nikhil on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Feng_WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
